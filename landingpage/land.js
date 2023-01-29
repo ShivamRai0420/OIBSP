f1ef6733ab785a596168c033fd34c321c226085d
Shivam Rai <96259644+ShivamRai0420@users.noreply.github.com>
2023-01-29 14:31:14 +0530

    Add files via upload

---
+++ landingpage/land.js
@@ -0,0 +1,18 @@
+gsap.from("#img1",{
+    delay:0.4,
+    opacity:0,
+    duration:2,
+    y:60
+})
+gsap.from("#img2",{
+    delay:0.4,
+    opacity:0,
+    duration:3,
+    x:60
+})
+gsap.from("#img3",{
+    delay:0.4,
+    opacity:0,
+    duration:4,
+    y:-60
+})
